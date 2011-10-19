0ff7719ac1109d6f5f827c65648545fc6b1e0794
d-matt <m@chezmatthi.eu>
2011-10-19 10:41:36 +0200

    clean some js code

--- js/patches.js
+++ js/patches.js
@@ -1,7 +1,5 @@
 jQuery(document).ready(function($){
   //Trick piwigo as tags page does not present a TITLE
-//  $("header").prepend($(".titrePage").html());
-//  $(".titrePage").remove();
     $(".titrePage h2").contents().appendTo("header h2");
     $(".titrePage .categoryActions").contents().appendTo("header .categoryActions");
     $(".titrePage").remove();
@@ -9,14 +7,9 @@ jQuery(document).ready(function($){
   //Move header notes as notifications in the content
   $(".header_notes").prependTo("#content").addClass("notice").show();
 
-  //Move error from header ro content 
+  //Move error from header to content 
   $(".error").prependTo("#content");
   $(".notice").prependTo("#content");
-
-  //Trick to expand content id on page without menubar (until piwigo 2.3)
-  //if ( ! $("#menubar").length ) {
-  //  $("#content").css("margin-left", "0");
-  //}
 
   //Trick to expand image_panel id on diaporamas
   if ( ! $("#imageInfoBar").length ) {
